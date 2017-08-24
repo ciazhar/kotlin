b5c2ec8b6f087264cd618663fb3c4f4c6ce803ca
ciazhar <daisetsunakama@gmail.com>
2017-08-24 19:58:55 +0700

    hello world

---
+++ src/basic/HelloWorld.kt
@@ -0,0 +1,18 @@
+package basic
+
+/**
+ * Created by ciazhar on 6/20/17.
+ */
+
+fun main(args : Array<String>){
+    //print some string
+    print("Hello World")
+
+    //variable declaration
+    val kata = "Halo"
+
+    //print value from variable
+    print(kata)
+
+
+}
