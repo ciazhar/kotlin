18735d1054dc3dcc02b0ea6b51623297531e36d4
ciazhar <daisetsunakama@gmail.com>
2017-08-24 20:00:51 +0700

    Import

---
+++ src/basic/Import.kt
@@ -0,0 +1,15 @@
+package basic
+
+///import secara umum
+import basic.utils.fungsi
+///import lalu dihanti nama
+import basic.utils.fungsiLain as lain
+
+/**
+ * Created by ciazhar on 8/24/17.
+ */
+
+fun main(args: Array<String>) {
+    fungsi()
+    lain()
+}
---
+++ src/basic/utils/SomeUtil.kt
@@ -0,0 +1,13 @@
+package basic.utils
+
+/**
+ * Created by ciazhar on 8/24/17.
+ */
+
+fun fungsi(){
+    print("Ini fungsi")
+}
+
+fun fungsiLain(){
+    print("Fungsi Lain")
+}
